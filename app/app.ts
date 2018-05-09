7b0ecdcb0e9deecafacd7e80ae3ae99fa41c77bb
Amer <amer.krlicbegovic@gmail.com>
2018-05-10 00:03:49 +0200

    Understanding Control Flow-based Type Analysis

--- app/app.ts
+++ app/app.ts
@@ -1,6 +1,16 @@
-function startGame(){
-    var messagesElement = document.getElementById('messages');
-    messagesElement.innerText = 'Welcome to MultiMath! Starting new game...';
+function startGame() {
+    // starting a new game
+
+    let playerName: string = 'Amer';
+    logPlayer(playerName);
+
+    let messagesElement: HTMLElement | null = document.getElementById('messages');
+    messagesElement!.innerText = 'Welcome to MultiMath! Starting new game...';
+    console.log('Starting new game.');
 }
 
-document.getElementById('startGame').addEventListener('click', startGame);
+function logPlayer(name) {
+    console.log(`New game starting for player: ${name}`);
+}
+
+document.getElementById('startGame')!.addEventListener('click', startGame);
