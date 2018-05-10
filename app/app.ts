82ee758f454aae7fc5d1dacbacf1f23907ec7f72
Amer <amer.krlicbegovic@gmail.com>
2018-05-10 17:23:32 +0200

    Adding Type Annotation

--- app/app.ts
+++ app/app.ts
@@ -1,16 +1,29 @@
 function startGame() {
     // starting a new game
 
-    let playerName: string = 'Amer';
+    let playerName: string | undefined = getInputValaue('playerName');
     logPlayer(playerName);
 
-    let messagesElement: HTMLElement | null = document.getElementById('messages');
-    messagesElement!.innerText = 'Welcome to MultiMath! Starting new game...';
-    console.log('Starting new game.');
+    postScore(100);
 }
 
-function logPlayer(name) {
+function logPlayer(name: string = 'MultiMath Player'): void {
     console.log(`New game starting for player: ${name}`);
+}
+
+function postScore(score: number, playerName: string ='MultiMath Player'): void {
+    let scoreElement: HTMLElement | null = document.getElementById('postedScore');
+    scoreElement!.innerHTML = `${score} - ${playerName}`;
+}
+
+function getInputValaue(elementID: string): string | undefined{
+    let inputElement: HTMLInputElement = <HTMLInputElement>document.getElementById(elementID);
+
+    if(inputElement.value === ''){
+        return undefined
+    } else{
+        return inputElement.value
+    }
 }
 
 document.getElementById('startGame')!.addEventListener('click', startGame);
--- js/app.js
+++ js/app.js
@@ -1,12 +1,25 @@
 function startGame() {
-    var playerName = 'Audrey';
+    var playerName = getInputValaue('playerName');
     logPlayer(playerName);
-    var messagesElement = document.getElementById('messages');
-    messagesElement.innerText = 'Welcome to MultiMath! Starting new game...';
-    console.log('Starting new game.');
+    postScore(100);
 }
 function logPlayer(name) {
+    if (name === void 0) { name = 'MultiMath Player'; }
     console.log("New game starting for player: " + name);
+}
+function postScore(score, playerName) {
+    if (playerName === void 0) { playerName = 'MultiMath Player'; }
+    var scoreElement = document.getElementById('postedScore');
+    scoreElement.innerHTML = score + " - " + playerName;
+}
+function getInputValaue(elementID) {
+    var inputElement = document.getElementById(elementID);
+    if (inputElement.value === '') {
+        return undefined;
+    }
+    else {
+        return inputElement.value;
+    }
 }
 document.getElementById('startGame').addEventListener('click', startGame);
 //# sourceMappingURL=app.js.map
