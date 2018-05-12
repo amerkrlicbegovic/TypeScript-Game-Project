2545fa136aed1ce037c240a41841fe292abd423d
Amer <amer.krlicbegovic@gmail.com>
2018-05-13 00:21:10 +0200

    Converting app to use modules

--- app/scoreboard.ts
+++ app/scoreboard.ts
@@ -1,6 +1,6 @@
-/// <reference path="result.ts" />
+import {Result} from './result';
 
-class Scoreboard {
+export class Scoreboard {
     private results: Result[] = [];
 
     addResult(newResult: Result): void {
@@ -8,9 +8,9 @@ class Scoreboard {
     }
 
     updateScoreboard(): void {
-    
+
         let output: string = '<h2>Scoreboard</h2>';
-    
+
         // loop over all results and create the html for the scoreboard
         for (let index = 0; index < this.results.length; index++) {
             let result: Result = this.results[index];
@@ -18,7 +18,7 @@ class Scoreboard {
             output += result.playerName + ': ' + result.score + '/' + result.problemCount + ' for factor ' + result.factor;
             output += '</h4>';
         }
-    
+
         // add the updated scoreboard to the page
         let scoresElement: HTMLElement = document.getElementById('scores')!;
         scoresElement.innerHTML = output;
